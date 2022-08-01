baff459832bf2de16429c8837772dcdd409a3dff
Mauricio Gutierrez <developer.mauricio2310@gmail.com>
2022-08-01 16:21:54 -0500

    ajustes

--- wp-content/themes/woodmart/header-elements/root.php
+++ wp-content/themes/woodmart/header-elements/root.php
@@ -1,4 +1,3 @@
 <div class="whb-main-header">
-	<?php echo do_shortcode( '[rev_slider alias="slider-1"][/rev_slider]' ); ?>
 	<?php echo apply_filters( 'woodmart_header_builder_root_children', $children ); ?>
 </div>
